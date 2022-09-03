f84357a8db944c1a31b04578cfbbff309b755ac9
Alister <xavieralister153@gmail.com>
2022-09-03 19:22:57 +0400

    first push

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 const __dirname1 = path.resolve();
 
