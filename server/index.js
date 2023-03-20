dcc2e6ee0610b5a4b2c970e255ff5696ab887fef
Alister <xavieralister153@gmail.com>
2023-03-20 13:38:05 +0400

    updatess

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 const __dirname1 = path.resolve();
 
-// app.use(express.static(path.join(__dirname1, "/client/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'), (err) => {
-//     if (err) res.status(500).send(err);
-//   });
-// });
+app.use(express.static(path.join(__dirname1, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'), (err) => {
+    if (err) res.status(500).send(err);
+  });
+});
 
 app.use(express.json());
 app.use(
