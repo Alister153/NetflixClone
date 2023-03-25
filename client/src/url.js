29ad7323520c29182a2931b1e88641959d79757d
Alister <xavieralister153@gmail.com>
2023-03-26 00:49:18 +0400

    localStorage

--- client/src/url.js
+++ client/src/url.js
@@ -1,5 +1,5 @@
 export const OriginalimgPATH = "https://image.tmdb.org/t/p/original/"
 export const w500imgPATH = "https://image.tmdb.org/t/p/w500/"
 export const YOUTUBE = "https://www.youtube.com/embed/"
-// export const baseUrl = ""
-export const baseUrl = "http://localhost:3001"
+export const baseUrl = ""
+// export const baseUrl = "http://localhost:3001"
